ced8eabac3730cf0d33fbfc1661d5f63669bbdb4
Jack Oliver <Jackoliver361@gmail.com>
2023-06-27 14:44:34 +0100

    updated cors

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ const routes = require('./routes/v1');
 const cookieParser = require("cookie-parser");
 var cors = require('cors')
 
-var corsOptions = {
-    origin: 'http://localhost:3080/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
 app.use('/api', routes);
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3080/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
         return res.status(401).json({ message: 'Invalid token', error: err.inner.message });
