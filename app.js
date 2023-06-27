58c997154111e8844fffe5220842a18e4072ee5d
Jack Oliver <Jackoliver361@gmail.com>
2023-06-27 19:04:39 +0100

    Updated cors settings

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ const routes = require('./routes/v1');
 const cookieParser = require("cookie-parser");
 var cors = require('cors')
 
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3080/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({
+    origin: 'http://localhost:3080',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
