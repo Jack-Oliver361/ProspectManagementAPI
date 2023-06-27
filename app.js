8199cbc8bdf57e74d9723558ad503ca5429f412a
Jack Oliver <Jackoliver361@gmail.com>
2023-06-27 14:48:50 +0100

    Fixed cors bug

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const cookieParser = require("cookie-parser");
 var cors = require('cors')
 
 
-
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api', routes);
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3080/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', routes);
+
 app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
         return res.status(401).json({ message: 'Invalid token', error: err.inner.message });
